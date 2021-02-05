a143ff6e79e6b34f2be1744792797780a7bd8099
Andrius Simanaitis <andrius@bei.lt>
2021-02-05 08:50:09 +0200

    added another kata

--- src/kata.ts
+++ src/kata.ts
@@ -6,3 +6,16 @@ export function descendingOrder(n: number): number {
     .join("");
   return +sortedStringNumber;
 }
+export function findMissingLetter(array: string[]): string {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const isInputLowercase = array[0].toLowerCase() === array[0];
+  const lowerCaseArray = array.map((e) => e.toLowerCase());
+  let inputIndex = 0;
+  let alphabetIndex = alphabet.indexOf(lowerCaseArray[0]);
+  while (lowerCaseArray[inputIndex] === alphabet[alphabetIndex]) {
+    alphabetIndex++;
+    inputIndex++;
+  }
+  const answer = alphabet[alphabetIndex];
+  return isInputLowercase ? answer : answer.toUpperCase();
+}
--- test/kata.test.ts
+++ test/kata.test.ts
@@ -1,9 +1,17 @@
-import {descendingOrder} from '../src/kata'
+import { descendingOrder, findMissingLetter } from "../src/kata";
 
-test('should descend string', () =>{
-    const testInputs = [0, 1, 42145, 145263, 123456789]
-    const testExpectations = [0, 1, 54421, 654321, 987654321]
-    testInputs.forEach((input, index) => {
-        expect(descendingOrder(input)).toBe(testExpectations[index])
-    })
-})
+test("should descend string", () => {
+  const testInputs = [0, 1, 42145, 145263, 123456789];
+  const testExpectations = [0, 1, 54421, 654321, 987654321];
+  testInputs.forEach((input, index) => {
+    expect(descendingOrder(input)).toBe(testExpectations[index]);
+  });
+});
+
+test("should find missing letter", () => {
+  const testInputs = [[..."abcdf".split("")], [..."OQRS".split("")]];
+  const testExpectations = ["e", "P"];
+  testInputs.forEach((input, index) => {
+    expect(findMissingLetter(input)).toBe(testExpectations[index]);
+  });
+});
